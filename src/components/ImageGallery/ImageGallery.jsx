7925d7d6ea3f757aa0f449a0b2c469c8d270696b
Alina-Zhulina <157721023+Alina-Zhulina@users.noreply.github.com>
2024-08-11 20:57:40 +0200

    Hw

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,7 +1,7 @@
 import ImageCard from "../ImageCard/ImageCard";
 import css from "./ImageGallery.module.css";
 
-const ImageGallery = ({ images }) => {
+const ImageGallery = ({ images, onImageClick }) => {
   if (images.length === 0) {
     return null;
   }
@@ -9,9 +9,11 @@ const ImageGallery = ({ images }) => {
     <ul className={css.gallery}>
       {images.map((image) => (
         <li key={image.id} className={css.galleryItem}>
-          <ImageCard image={image} />
+          <ImageCard image={image} onClick={() => onImageClick(image)} />
         </li>
       ))}
     </ul>
   );
 };
+
+export default ImageGallery;
